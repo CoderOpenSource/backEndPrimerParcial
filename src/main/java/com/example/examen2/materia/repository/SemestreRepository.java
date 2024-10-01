89094b76bae448da5d4fbefefdb5ae7725ebf7c1
unknown <adrianysamuel4ever@gmail.com>
2024-10-01 18:59:02 -0400

    prueba

--- src/main/java/com/example/examen2/materia/repository/SemestreRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.examen2.materia.repository;
-import com.example.examen2.materia.model.Semestre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SemestreRepository extends JpaRepository<Semestre, Long> {
-}
